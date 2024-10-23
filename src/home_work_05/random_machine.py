8af0eea37935c11b155bbbe16001a9d53f5a2d5e
baga <www.baglan888@gmail.com>
2024-10-23 21:51:30 +0500

    added ruff

--- src/home_work_05/random_machine.py
+++ src/home_work_05/random_machine.py
@@ -1,15 +1,17 @@
 import random
 from typing import List
 
+
 def is_prime(x: int) -> bool:
     if x < 2:
         return False
-    
+
     for i in range(2, int(x * 0.5) + 1):
         if x % i == 0:
             return False
-    
+
     return True
+
 
 def primes(count: int) -> List[int]:
     primes = []
@@ -20,14 +22,16 @@ def primes(count: int) -> List[int]:
         num += 1
     return primes
 
+
 def checksum(x: List[int]) -> int:
     ini_sum = 0
     for i in x:
         ini_sum = ini_sum + i
         ini_sum = ini_sum * 113
         ini_sum = ini_sum % 10000007
-    
+
     return ini_sum
+
 
 def pipeline() -> int:
     primes_list = primes(1000)
@@ -35,6 +39,7 @@ def pipeline() -> int:
     random.shuffle(primes_list)
     res = checksum(primes_list)
     return res
+
 
 if __name__ == "__main__":
     print(pipeline())
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,19 +1,24 @@
 from src.home_work_05.random_machine import is_prime, primes, checksum, pipeline
 
+
 def test_is_prime():
-    assert is_prime(1) == False
-    assert is_prime(-3) == False
-    assert is_prime(3) == True
-    assert is_prime(17) == True
+    assert is_prime(1) == 0
+    assert is_prime(-3) == 0
+    assert is_prime(3) == 1
+    assert is_prime(17) == 1
+
 
 def test_primes():
     assert primes(2) == [2, 3]
 
+
 def test_checksum():
     assert checksum([1, 2, 6, 24]) == 6012369
 
+
 def test_pipline():
     assert pipeline() == 7785816
+
 
 if __name__ == "__main__":
     test_is_prime()
